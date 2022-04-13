feb4075e31f09d8841135ecf92b84221d48f500e
Maulana Akmal <maulana.akmal@shopee.com>
2022-04-14 02:02:56 +0800

    Add dummy data

---
+++ src/components/Table/data.js
@@ -0,0 +1,126 @@
+const data = {
+  reservations: [
+    {
+      reservation_id: "id1",
+      occupier_id: "occupier1",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id2",
+      occupier_id: "occupier2",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id3",
+      occupier_id: "occupier3",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id4",
+      occupier_id: "occupier4",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id5",
+      occupier_id: "occupier5",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id1",
+      occupier_id: "occupier1",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id2",
+      occupier_id: "occupier2",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id3",
+      occupier_id: "occupier3",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id4",
+      occupier_id: "occupier4",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id5",
+      occupier_id: "occupier5",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id1",
+      occupier_id: "occupier1",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id2",
+      occupier_id: "occupier2",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id3",
+      occupier_id: "occupier3",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id4",
+      occupier_id: "occupier4",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id5",
+      occupier_id: "occupier5",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id1",
+      occupier_id: "occupier1",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id2",
+      occupier_id: "occupier2",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id3",
+      occupier_id: "occupier3",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id4",
+      occupier_id: "occupier4",
+      status: "active",
+      start_time: 12312321313,
+    },
+    {
+      reservation_id: "id5",
+      occupier_id: "occupier5",
+      status: "active",
+      start_time: 12312321313,
+    },
+  ],
+};
+
+export { data };
